349d05b2f94e30485832751b710a3a70924d4c1f
mie-intel <paryapradhanika@gmail.com>
2025-05-22 15:49:15 +0700

    fix(supabase)

---
+++ fe/src/utils/supabase/client.js
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+export async function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase URL or Anon Key is not defined");
+  }
+
+  return createClient(supabaseUrl, supabaseAnonKey);
+}
